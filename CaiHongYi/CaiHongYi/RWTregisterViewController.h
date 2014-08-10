f0bbb22a5c02615bfee0cdc59481b84ced90dfe6
peterzcc <peterz@Peters-MacBook-Air.local>
2014-08-10 12:10:45 +0800

    asd

---
+++ CaiHongYi/CaiHongYi/RWTregisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWTregisterViewController.h
+//  CaiHongYi
+//
+//  Created by Peter Z on 10/8/14.
+//  Copyright (c) 2014 Peter Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTregisterViewController : UIViewController
+
+@end
